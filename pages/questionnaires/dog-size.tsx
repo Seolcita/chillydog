bb2f9ee78478c6826822cb80a0b527e7348da5c5
Seolcita <seolcita@gmail.com>
2023-12-20 10:39:04 -0700

    draft

--- pages/questionnaires/dog-size.tsx
+++ pages/questionnaires/dog-size.tsx
@@ -2,12 +2,24 @@ import { ReactElement } from 'react';
 import { Questionnaire } from '../../components/Questionnaire/Questionnaire';
 import { DogSizeForm } from '../../components/Screens/DogSize/DogSizeForm';
 
-const DogSizeScreen = (): ReactElement => {
-  const question = `Q. What is your dog's size?`;
+const DogSizeScreen = (props: { question: string }): ReactElement => {
+  // const question = `Q. What is your dog's size?`;
 
   return (
-    <Questionnaire currentStep={2} question={question} form={<DogSizeForm />} />
+    <Questionnaire
+      currentStep={2}
+      question={props.question}
+      form={<DogSizeForm />}
+    />
   );
 };
+
+export function getStaticProps() {
+  return {
+    props: {
+      question: `Q. What is your dog's size?`,
+    },
+  };
+}
 
 export default DogSizeScreen;
