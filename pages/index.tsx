2775eb8028da745ae38533e27700d35dd9b307fb
Seolcita <seolcita@gmail.com>
2023-12-20 16:23:05 -0700

    fix build issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import { ReactElement } from 'react';
 import NameScreen from './questionnaires/name';
-import ColdAdaptScreen from './questionnaires/cold-adapt';
 
 //TODO: It is placeholder. It will be replaced
 const Home = (): ReactElement => {
-  return <ColdAdaptScreen />;
+  return <NameScreen />;
 };
 
 export default Home;
