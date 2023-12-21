fd6aba4b6eace933322af79012b558522f7404de
Seolcita <seolcita@gmail.com>
2023-12-20 19:12:39 -0700

    added complete screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from 'react';
 import NameScreen from './questionnaires/name';
+import CompleteScreen from './questionnaires/complete';
 
 //TODO: It is placeholder. It will be replaced
 const Home = (): ReactElement => {
-  return <NameScreen />;
+  return <CompleteScreen />;
 };
 
 export default Home;
