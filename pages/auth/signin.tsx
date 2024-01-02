2097a0d70a866f5f3e825df8a74d645e62706a6d
Seolcita <seolcita@gmail.com>
2024-01-01 17:43:03 -0700

    removed comment out

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,35 +1,5 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from 'next';
-// import { getProviders, signIn } from 'next-auth/react';
-// import { getServerSession } from 'next-auth/next';
-// import { authOption } from '../api/auth/[...nextauth]';
 import Login from '../../components/Login/Login';
-import UserContext from '../../context/user.context';
-import { useContext } from 'react';
 
 export default function SignIn() {
   return <Login />;
 }
-
-// export async function getServerSideProps(context: GetServerSidePropsContext) {
-//   // const session = await getServerSession(context.req, context.res, authOption);
-
-//   // if (session) {
-//   //   return { redirect: { destination: '/' } };
-//   // }
-
-//   // const providers = await getProviders();
-//   const userCtx = useContext(UserContext);
-//   // const isLoggedIn = userCtx.user !== null;
-
-//   if (userCtx.user !== null) {
-//     return { redirect: { destination: '/' } };
-//   }
-
-//   return {
-//     props: {},
-//   };
-// }
