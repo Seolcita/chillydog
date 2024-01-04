2b377fe93bc0bbbf12536d5a4a3bba577160791a
Seolcita <seolcita@gmail.com>
2024-01-04 09:26:24 -0700

    fixing

--- pages/questionnaires/dog-size.tsx
+++ pages/questionnaires/dog-size.tsx
@@ -1,13 +1,12 @@
 import { ReactElement } from 'react';
 import { Questionnaire } from '../../components/Questionnaire/Questionnaire';
 import { DogSizeForm } from '../../components/Screens/DogSize/DogSizeForm';
+import { Utill } from '../../components/Screens/Util';
 
 const DogSizeScreen = (): ReactElement => {
   const question = `Q. What is your dog's size?`;
 
-  return (
-    <Questionnaire currentStep={2} question={question} form={<DogSizeForm />} />
-  );
+  return <Utill />;
 };
 
 export default DogSizeScreen;
