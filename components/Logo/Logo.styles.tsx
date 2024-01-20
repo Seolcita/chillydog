82c652a17bd42f429b6a2ea2156b7d3e571c2ad8
Seolcita <seolcita@gmail.com>
2024-01-19 20:08:26 -0700

    Updated Logo title size for mobile size

--- components/Logo/Logo.styles.tsx
+++ components/Logo/Logo.styles.tsx
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 import ColorMap from '../../styles/Color';
 
-export const LogoText = styled.span`
+interface LogoTextProps {
+  $isMobile: boolean;
+}
+
+export const LogoText = styled.span<LogoTextProps>`
   margin-left: 1.5rem;
-  font-size: 3.5rem;
+  font-size: ${({ $isMobile }) => ($isMobile ? '3rem' : '3.5rem')};
   color: ${ColorMap['white'].main};
 `;
--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -4,8 +4,11 @@ import Image from 'next/image';
 
 import * as S from './Logo.styles';
 import { lilita } from '../../styles/Fonts';
+import { DeviceType, useWindowSize } from '../../hooks/use-window-resize';
 
 export const Logo = (): ReactElement => {
+  const { deviceType } = useWindowSize();
+  const isMobile = deviceType === DeviceType.MOBILE;
   return (
     <Box display='flex' alignItems='center'>
       <Image
@@ -15,7 +18,9 @@ export const Logo = (): ReactElement => {
         height={50}
         style={{ border: '0.2rem solid white', borderRadius: '50%' }}
       />
-      <S.LogoText className={lilita.className}>Chilly Dog</S.LogoText>
+      <S.LogoText className={lilita.className} $isMobile={isMobile}>
+        Chilly Dog
+      </S.LogoText>
     </Box>
   );
 };
