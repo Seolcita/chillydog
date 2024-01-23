5226df98dc007c0667f1e6911dd0d17066660026
Seolcita <seolcita@gmail.com>
2024-01-23 15:34:09 -0700

    ready

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/dist/shared/lib/constants');
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-  // const env = {
-  //   END_POINT_URL: isDev
-  //     ? 'http://localhost:3001/api'
-  //     : 'https://nice-blue-gosling-kit.cyclic.app/api', //TODO: change to production url once it is ready
-  // };
+  const env = {
+    END_POINT_URL: isDev
+      ? 'http://localhost:3001/api'
+      : 'https://nice-blue-gosling-kit.cyclic.app/api', //TODO: change to production url once it is ready
+  };
 
-  const env = { END_POINT_URL: 'https://nice-blue-gosling-kit.cyclic.app/api' };
+  // const env = { END_POINT_URL: 'https://nice-blue-gosling-kit.cyclic.app/api' };
   // const env = { END_POINT_URL: 'http://localhost:3001/api' };
 
   return {
